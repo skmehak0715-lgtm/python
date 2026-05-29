987620e491de6b2898d867b61c1f2c1cb0255332
skmehak0715-lgtm <sk.mehak0715@gmail.com>
2026-05-29 14:07:02 +0530

    Add files via upload

---
+++ class3/data type.py
@@ -0,0 +1,9 @@
+#data types
+a = 345
+print("the data type of a is ",type(a))
+name = "Mahek"
+print("the data type of a is ",type(name))
+height = 78.90
+print("the data type of a is ",type(height))
+isstudent = False
+print("the data type of a is ",type(isstudent))
---
+++ class3/type casting.py
@@ -0,0 +1,18 @@
+#types casting
+a = 345
+print("the data type of a is ",type(a))
+name = "Mahek"
+print("the data type of name is ",type(name))
+height = 78.90
+print("the data type of height is ",type(height))
+isstudent = False
+print("the data type of isstudent is ",type(isstudent))
+
+a = float(a)
+print("the data type of a is",print(a))
+height = int(height)
+print(height)
+print("the data type of height is",type(height))
+isstudent = int(isstudent)
+print(isstudent)
+print("the data type of isstudent is",type(isstudent))
