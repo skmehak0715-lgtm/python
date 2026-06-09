6c6866dfe0429b4b999ccbaf9d1fa21257c6fd5b
skmehak0715-lgtm <sk.mehak0715@gmail.com>
2026-06-09 15:24:55 +0530

    Add files via upload

---
+++ HW2/and or operator.py
@@ -0,0 +1,15 @@
+# and or operator
+a = 10
+b = 12
+c = 0
+if a and b and c:
+     print("all the number have boolean value true")
+else:
+     print("one of the variables have boolean value false")
+
+     a = 10
+     b = -10
+if a > 0 or b > 0:
+          print("either of the varibles is greather than zero")
+else:
+    print("none of the varibles is gereather than zero.")
