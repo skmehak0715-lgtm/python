bd670b27f4df8776bee7e5706d7ee12ae3e62f06
skmehak0715-lgtm <sk.mehak0715@gmail.com>
2026-09-09 21:23:24 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,16 @@
+try:
+    def add (a,b):
+        return(a + b) 
+
+    def sup (a,b):
+        return(a - b)
+
+    def mul (a,b):
+        return(a * b)
+
+    def div (a,b):
+        return(a / b)
+
+    a = int(input("a"))
+    b = int(input("b"))
+    c = input ("")
