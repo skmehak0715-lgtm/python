0196863c77d22a26f9f2f1643b855da978a74b87
skmehak0715-lgtm <sk.mehak0715@gmail.com>
2026-09-14 21:34:26 +0530

    Add files via upload

---
+++ class17/list1.py
@@ -0,0 +1,14 @@
+# list
+empty_list = []
+print(empty_list)
+
+# list number
+numbers = [7 , 8, 9, 4, 3]
+print(numbers)
+
+triples = [7 , 8, 9] * 4
+print(triples)
+
+alist = [ 700 , 800 , 900 , 400 , 300 ]
+alist = alist [::-1]
+print(alist,"/n")
