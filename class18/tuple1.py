7311c1f45df328fd9772d36ab82a62a1514a530d
skmehak0715-lgtm <sk.mehak0715@gmail.com>
2026-09-16 21:37:50 +0530

    Add files via upload

---
+++ class18/tuple1.py
@@ -0,0 +1,18 @@
+# tuple operater
+tuplex = ("tuple",False,3.2,1)
+print(tuplex)
+
+tuplex = (5,7,9,4,3,6)
+print(tuplex)
+
+tuple = tuplex + (7,)
+print(tuplex)
+
+tuple1 = (90,70,80,40,20)
+print(tuple1.count(50))
+
+tuplex = (7,8,9,4,3,2,1,5,6,9)
+slice = tuplex[3:5]
+print( slice)
+_slice = tuplex[ :6]
+print( slice)
